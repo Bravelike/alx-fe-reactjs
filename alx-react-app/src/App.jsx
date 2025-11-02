ed36fbda0921e450eb62d7c482c47ccfc90560b9
Bravelike <bunybravelike23@gmail.com>
2025-11-02 22:09:21 +0100

    little fix

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
       <UserProfile
         name="Alice"
         age="25"
-        bio = "SLoves hiking and photography"
+        bio = "Loves hiking and photography"
       />
     </>
   )
