38ef0cb1654c3924d1e091466fe0d9d6f9f1fc09
Bravelike <bunybravelike23@gmail.com>
2025-11-10 14:26:26 +0100

    styling components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor: 'orange'}}>
+            <p style={{color: 'aliceblue'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
