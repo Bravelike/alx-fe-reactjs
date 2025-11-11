5801c38b8f940972633313b2ff54205734f2e647
Bravelike <bunybravelike23@gmail.com>
2025-11-11 21:56:36 +0100

    dklnfmd

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{backgroundColor:'navy'}}>
+            <p style={{fontSize:'14px', fontWeight: 'bold'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+// Create the context
+const UserContext = createContext();
+
+// Export it so other components can use it
+export default UserContext;
