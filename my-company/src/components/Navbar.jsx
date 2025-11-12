9702c8dbf96b5ea9d74086ae01a9975ac7ae9094
Bravelike <bunybravelike23@gmail.com>
2025-11-12 03:29:58 +0100

    fixed another bug

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,31 @@
-import { Link } from 'react-router-dom';
+
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
-      <Link to="/" style={{ color: '#fff', margin: '0 15px', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ color: '#fff', margin: '0 15px', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ color: '#fff', margin: '0 15px', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ color: '#fff', margin: '0 15px', textDecoration: 'none' }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: "#333",
+        padding: "10px",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
+
 export default Navbar;
