f37b8f3684dfcb862ee4262997091688e21221d6
Bravelike <bunybravelike23@gmail.com>
2025-12-12 23:08:45 +0100

    bew

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,5 +1,5 @@
  module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
